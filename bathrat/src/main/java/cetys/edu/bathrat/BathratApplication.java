f59898f631d0630eca1dc6a2d6fcbbc5ee718dd5
Adrian <adrian@MacBook-Pro-de-Adrian.local>
2023-11-14 20:40:26 -0800

    Created java project

---
+++ bathrat/src/main/java/cetys/edu/bathrat/BathratApplication.java
@@ -0,0 +1,13 @@
+package cetys.edu.bathrat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BathratApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BathratApplication.class, args);
+	}
+
+}
---
+++ bathrat/src/test/java/cetys/edu/bathrat/BathratApplicationTests.java
@@ -0,0 +1,13 @@
+package cetys.edu.bathrat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BathratApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
